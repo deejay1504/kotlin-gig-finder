12f432d00f985e0de84f1ca3a5670ef0267c5b55
Dee Jay <dee.jay1@gmail.com>
2019-11-02 17:16:16 +0000

    Added tests

--- src/test/kotlin/org/springframework/gigfinder/system/HomeControllerTest.kt
+++ src/test/kotlin/org/springframework/gigfinder/system/HomeControllerTest.kt
@@ -1,6 +1,7 @@
 package org.springframework.gigfinder.system
 
 
+import org.hamcrest.Matchers
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
@@ -14,6 +15,7 @@ import org.springframework.gigfinder.service.GigService
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 
 @ExtendWith(SpringExtension::class)
@@ -36,16 +38,41 @@ class HomeControllerTest {
         gigDetailsForm.totalEntries = 1
         gigDetailsForm.resultsPerPage= 20
         gigDetailsForm.metroAreaId = 24555
+        gigDetailsForm.gigLocation = "Brighton"
+        gigDetailsForm.gigStartDate = "02-11-2019"
+        gigDetailsForm.gigEndDate = "02-11-2019"
         gigDetailsForm.gigList = ArrayList<GigDetails>()
         given(gigService.createGigDetails()).willReturn(gigDetailsForm)
     }
 
     @Test
-    fun testInitCreationForm() {
+    fun shouldCreateGigDetailsForm() {
         mockMvc.perform(get("/"))
                 .andExpect(status().isOk)
                 .andExpect(model().attributeExists("gigDetailsForm"))
                 .andExpect(view().name(VIEWS_GIG_DETAILS_FORM))
     }
+
+    /**
+    @Test
+    fun shouldReturnGigListInForm() {
+
+        mockMvc.perform(post("/gigfinder")
+                        .param("totalEntries", "1")
+                        .param("gigLocation", "Brighton")
+                        .param("metroAreaId", "24555")
+                        .param("gigStartDate", "02-11-2019")
+                        .param("gigEndDate", "02-11-2019")
+                        .param("currentPage", "02-11-2019")
+                        .param("resultsPerPage", "20")
+                        .param("numberOfPages", "0")
+                )
+                .andExpect(status().isOk)
+                .andExpect(model().attributeExists("gigDetailsForm"))
+                .andExpect(model().attribute("gigDetailsForm", Matchers.hasProperty<Any>("gigStartDate", Matchers.`is`("Franklin"))))
+                .andExpect(model().attribute("gigDetailsForm", Matchers.hasProperty<Any>("gigEndDate", Matchers.`is`("Franklin"))))
+                .andExpect(view().name(VIEWS_GIG_DETAILS_FORM))
+    }
+    */
 
 }
